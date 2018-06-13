c92a9bc314a1bd2a56c1542dcc9190005130a7d1
Denis Kutsokon <denis.kutsokon@gmail.com>
2018-06-14 00:00:01 +0300

    add logger, change project start

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
-import chalk from "chalk";
-import express from "express";
-import mongoose from "mongoose";
-import { Routes } from "./routes";
+import chalk from 'chalk';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { Routes } from './routes';
+import logger from './utils/logger';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoURI = "mongodb://localhost:27017/totopal";
+const mongoURI = 'mongodb://localhost:27017/totopal';
 
 mongoose.connect(mongoURI);
-mongoose.connection.on("error", err => {
-  console.error(err);
-  console.log(
-    "%s MongoDB connection error. Please make sure MongoDB is running.",
-    chalk.red("✗")
-  );
+mongoose.connection.on('error', (err) => {
+  logger.error(err.message);
+  logger.info('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
 });
 
 app.use(express.json());
-app.use("/v1", Routes);
+app.use('/v1', Routes);
 
 app.listen(PORT, () => {
-  console.log(`this server is listening on ${PORT}`);
+  logger.info(`this server is listening on ${PORT}`);
 });
